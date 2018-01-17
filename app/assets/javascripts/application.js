a87e7aa92f3ec2211e8b39b343c2dce098cc8875
weifanhaha <aily34809@gmail.com>
2018-01-17 14:28:24 +0800

    jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+//= require jquery
+//= require jquery_ujs
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
-//= require jquery
-//= require jquery_ujs
 //= require bootstrap
 //= require summernote
