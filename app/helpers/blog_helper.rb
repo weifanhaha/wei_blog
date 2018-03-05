e6dc826ede72a1dcdb6284ea33077504bd740a2a
weifanhaha <aily34809@gmail.com>
2018-03-05 13:03:25 +0800

    category btn and hide test

--- app/helpers/blog_helper.rb
+++ app/helpers/blog_helper.rb
@@ -13,6 +13,29 @@ module BlogHelper
       content = content[p_end..-1]
       count += 1
     end
-    return new_content.truncate(50)
+    new_content.truncate(50)
   end
+
+  def valid_blogs
+    v_blogs = Blog.all.to_a
+    v_blogs.each do |blog|
+      p 'loop'
+      if blog.category.include?"test" or blog.category.include?"隱藏" or blog.category.include?"草稿"
+        v_blogs.delete(blog)
+      end
+    end
+    v_blogs
+  end
+
+  def invalid_blogs
+    inv_blogs = []
+    Blog.all.each do |blog|
+      p 'loop'
+      if blog.category.include?"test" or blog.category.include?"隱藏" or blog.category.include?"草稿"
+        inv_blogs.append(blog)
+      end
+    end
+    inv_blogs
+  end
+
 end
